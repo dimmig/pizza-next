55464bccd8eaf9003745f944cbcc05b263b55137
D. Galagan <dima@airtrail.co>
2025-02-24 00:53:12 +0100

    added missing components

---
+++ shared/components/shared/cartButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Button} from "@/shared/components/ui/button";
+import {ArrowRight, ShoppingCart} from "lucide-react";
+import {CartDrawer} from "@/shared/components/shared/cart-drawer";
+
+interface Props {
+    className?: string
+}
+export const CartButton: React.FC<Props> = ({ className }) => {
+    return (
+        <CartDrawer>
+            <Button className='group relative'>
+                <b>120$</b>
+                <span className='h-full w-[1px] bg-white/30 mx-3'/>
+                <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+                    <ShoppingCart className='h-4 w-4 relative' strokeWidth={2}/>
+                    <b>2</b>
+                </div>
+                <ArrowRight
+                    className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:-translate-x-0'/>
+            </Button>
+        </CartDrawer>
+    )
+}
--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -6,6 +6,7 @@ import {Button} from "@/shared/components/ui/button";
 import {ArrowRight, ShoppingCart, User} from "lucide-react";
 import Link from "next/link";
 import {SearchInput} from "@/shared/components/shared/searchInput";
+import {CartButton} from "@/shared/components/shared/cartButton";
 
 
 interface Props {
@@ -39,18 +40,7 @@ export const Header: React.FC<Props> = ({className}) => {
                         Log in
                     </Button>
 
-                    <div>
-                        <Button className='group relative'>
-                            <b>120$</b>
-                            <span className='h-full w-[1px] bg-white/30 mx-3'/>
-                            <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
-                                <ShoppingCart className='h-4 w-4 relative' strokeWidth={2}/>
-                                <b>2</b>
-                            </div>
-                            <ArrowRight
-                                className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:-translate-x-0'/>
-                        </Button>
-                    </div>
+                        <CartButton />
                 </div>
             </Container>
         </div>
